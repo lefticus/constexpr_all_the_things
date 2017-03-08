d7dfb5baf53539e508778a92090250651b18e6df
Jason Turner <jason@emptycrate.com>
2017-03-07 17:39:49 -0800

    Tear out recursive JSON_Value

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -11,29 +11,8 @@
 
 #include <iostream>
 
-constexpr auto get_json_value()
-{
-  // we can elide the <> in C++17 with class template type deduction
-  cx::JSON_Value j{};
-  j["a"].to_Number() = 15;
-  j["b"].to_String() = "Hello World";
-  j["d"].to_Array();
-  j["c"]["b"].to_Array().push_back(10.0);
-  j["c"]["a"] = cx::static_string("Hello World");
-  j["c"]["a"].to_Array().push_back(5.2);
-  return j;
-}
-
 int main(int, char *[])
 {
-  constexpr auto json_value = get_json_value();
-  static_assert(json_value["c"]["a"][0].to_Number() == 5.2);
-
-  static_assert(json_value["b"].to_String().size() == 11);
-  static_assert(cx::static_string("Hello World").size() == 11);
-  static_assert(json_value["b"].to_String() == "Hello World");
-  
-
   using namespace std::literals;
 
   {
@@ -142,76 +121,6 @@ int main(int, char *[])
         return v.size();
       };
     static_assert(f() == 6);
-  }
-
-  {
-    // test JSON values
-
-    constexpr auto true_val = JSON::value_parser<>()("true"sv);
-    static_assert(true_val && true_val->first.to_Boolean());
-
-    constexpr auto false_val = JSON::value_parser<>()("false"sv);
-    static_assert(false_val && !false_val->first.to_Boolean());
-
-    constexpr auto null_val = JSON::value_parser<>()("null"sv);
-    static_assert(null_val && null_val->first.is_Null());
-
-    constexpr auto number_val = JSON::value_parser<>()("1.23"sv);
-    static_assert(number_val && number_val->first.to_Number() == 1.23);
-
-    constexpr auto str_val = JSON::value_parser<>()(R"("hello")"sv);
-    static_assert(str_val && str_val->first.to_String() == "hello");
-
-    {
-      constexpr auto object_val = JSON::value_parser<>()("{}"sv);
-      static_assert(object_val && object_val->first.to_Object().empty());
-    }
-
-    {
-      constexpr auto object_val = JSON::value_parser<>()(R"({"a":1})"sv);
-      static_assert(object_val && object_val->first["a"].to_Number() == 1);
-    }
-
-    {
-      constexpr auto object_val = JSON::value_parser<>()(R"({"a":1,"b":true,"c":{}})"sv);
-      static_assert(object_val
-                    && object_val->first.to_Object().size() == 3
-                    && object_val->first["a"].to_Number() == 1
-                    && object_val->first["b"].to_Boolean()
-                    && object_val->first["c"].to_Object().empty());
-    }
-
-    {
-      constexpr auto array_val = JSON::value_parser<>()("[]"sv);
-      static_assert(array_val && array_val->first.to_Array().empty());
-    }
-
-    {
-      constexpr auto array_val = JSON::value_recur::array_parser()(
-          R"([1,null,true,[2],{},"hello"])"sv);
-      static_assert(array_val
-                    && array_val->first[0].to_Number() == 1
-                    && array_val->first[1].is_Null()
-                    && array_val->first[2].to_Boolean()
-                    && array_val->first[3][0].to_Number() == 2
-                    && array_val->first[4].to_Object().empty()
-                    && array_val->first[5].to_String() == "hello");
-    }
-
-    {
-      using namespace JSON::literals;
-      constexpr auto val = R"( [
-                                 1 , null , true , [ 2 ] ,
-                                 { "a" : 3.14 } , "hello"
-                               ] )"_json;
-      static_assert(val
-                    && val->first[0].to_Number() == 1
-                    && val->first[1].is_Null()
-                    && val->first[2].to_Boolean()
-                    && val->first[3][0].to_Number() == 2
-                    && val->first[4]["a"].to_Number() == 3.14
-                    && val->first[5].to_String() == "hello");
-    }
   }
 
   {
